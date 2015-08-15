a0e7d45836d4ab04050cb08cca6eb22ae2adbb8a
EmilyPlatzer <emilyplatzer@gmail.com>
2015-08-15 12:04:41 -0600

    User can calculate amount per person
    given subtotal, tip percentage, number of people

--- playground.clj
+++ playground.clj
@@ -3,3 +3,9 @@
   [subtotal tip-pct]
   ( * 1.08 subtotal ( + 1 tip-pct ) )
 )
+
+(defn share-per-person
+  "Given a subtotal, tip percentage, and number of people, use total-with-tip to return amount per person"
+  [subtotal tip-pct number-guests]
+  ( / ( total-with-tip subtotal tip-pct ) number-guests )
+)
